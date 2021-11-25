412eee40d01c3381c957722c19250dc72d16f8ab
hichamm <hishamaskour@gmail.com>
2021-11-25 21:20:47 +0100

    init commit with header

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Button, Header } from "react-native-elements";
+import { StatusBar } from "react-native";
+import AppHeader from "./components/Header";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View>
+        <StatusBar backgroundColor={"#EB7660"} />
+        <AppHeader />
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Header.js
@@ -0,0 +1,51 @@
+export default function AppHeader({ props }) {
+  return (
+    <View style={styles.headerStyle}>
+      <Text style={styles.searchTitle}>Search</Text>
+      <View style={styles.textInputContainerStyle}>
+        <Image
+          style={styles.searchIconStyle}
+          source={require("../assets/ico_search_off.png")}
+        />
+        <TextInput
+          placeholder="Search"
+          editable
+          style={styles.textInputStyle}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: "#EB7660",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  searchTitle: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  textInputStyle: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    paddingLeft: 45,
+  },
+  textInputContainerStyle: {
+    position: "relative",
+  },
+  searchIconStyle: {
+    position: "absolute",
+    zIndex: 9,
+    top: 14,
+    left: 15,
+  },
+});
