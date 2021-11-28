c3df9e497a18bbca20cdc54ee741f031a9e6c7bd
hichamm <hishamaskour@gmail.com>
2021-11-28 17:25:47 +0100

    refactor movie detail screen

--- screens/MovieDetailScreen.js
+++ screens/MovieDetailScreen.js
@@ -6,13 +6,13 @@ import {
   Dimensions,
   ScrollView,
   Pressable,
-  Button,
-  TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 import { StatusBar } from "react-native";
 import axios from "../services/axiosConfig";
 import Rating from "../components/Rating";
-export default function MovieDetailScreen({ route, navigation }) {
+import MvButton from "../components/MvButton";
+export default function MovieDetailScreen({ route }) {
   const { id } = route.params;
   const [movie, setMovie] = useState();
   const screen = Dimensions.get("screen");
@@ -37,122 +37,106 @@ export default function MovieDetailScreen({ route, navigation }) {
 
       {movie && (
         <ScrollView>
-          <View style={{ position: "relative" }}>
-            <View
-              style={{
-                borderBottomLeftRadius: 30,
-                position: "relative",
-              }}
-            >
-              <View
-                style={{
-                  borderBottomLeftRadius: 30,
-                  overflow: "hidden",
-                  position: "relative",
+          <View>
+            <View style={styles.mainPosterImageStyle}>
+              <Image
+                source={{
+                  uri:
+                    "https://image.tmdb.org/t/p/original" + movie.poster_path,
+                  height: 100,
+                  width: screen.width,
+                  height: screen.height * 0.6,
                 }}
-              >
+              />
+            </View>
+            <View style={styles.subPosterContainerStyle}>
+              <View style={styles.thumbnailStyle}>
                 <Image
                   source={{
                     uri:
                       "https://image.tmdb.org/t/p/original" + movie.poster_path,
-                    height: 100,
-                    width: screen.width,
-                    height: screen.height * 0.6,
+                    width: 100,
+                    height: 150,
                   }}
                 />
               </View>
-
-              <View
-                style={{
-                  position: "absolute",
-                  bottom: -120,
-                  left: 25,
-                  borderWidth: 8,
-                  borderColor: "#fff",
-                  borderRadius: 20,
-                  zIndex: 99,
-                }}
-              >
-                <View
-                  style={{
-                    borderRadius: 20,
-                    overflow: "hidden",
-                    backgroundColor: "#000",
-                  }}
-                >
-                  <Image
-                    source={{
-                      uri:
-                        "https://image.tmdb.org/t/p/original" +
-                        movie.poster_path,
-                      width: 100,
-                      height: 150,
-                    }}
-                  />
-                </View>
-              </View>
-
-              <Pressable
-                style={{ position: "absolute", bottom: -30, right: 20 }}
-              >
-                <Image source={require("../assets/button_play.png")} />
-              </Pressable>
             </View>
+            <Pressable style={styles.playButtonStyle}>
+              <Image source={require("../assets/button_play.png")} />
+            </Pressable>
+          </View>
 
-            <View
-              style={{
-                paddingLeft: 165,
-                paddingTop: 30,
-                paddingHorizontal: 30,
-              }}
+          <View style={styles.movieHeaderInfoStyle}>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              {movie.title}
+            </Text>
+            <Text style={{ color: "#555", fontSize: 18 }}>
+              Francis ford coppola
+            </Text>
+            <Rating range={movie.vote_average / 2} />
+          </View>
+
+          <View style={styles.synopsisStyle}>
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}
             >
-              <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-                {movie.title}
-              </Text>
-              <Text style={{ color: "#555", fontSize: 18 }}>
-                Francis ford coppola
-              </Text>
-              <Rating range={movie.vote_average / 2} />
-            </View>
-
-            <View style={{ paddingTop: 40, paddingHorizontal: 25, paddingBottom: 120 }}>
-              <Text
-                style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}
-              >
-                Synopsis
-              </Text>
-              <Text style={{ fontSize: 16, color: "#999" }}>
-                {movie.overview}
-              </Text>
-            </View>
+              Synopsis
+            </Text>
+            <Text style={{ fontSize: 16, color: "#999" }}>
+              {movie.overview}
+            </Text>
           </View>
         </ScrollView>
       )}
-      <View
-        style={{
-          position: "absolute",
-          bottom: 0,
-          // height: 50,
-          width: screen.width,
-          backgroundColor: "#fff",
-          paddingHorizontal: 20,
-          paddingVertical: 20
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            height: 50,
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: '#EB7660',
-            borderRadius: 15
-      
-          }}
-        >
-          <Text style={{ color: "#fff", fontSize: 18 }}>Visit website</Text>
-        </TouchableOpacity>
+      <View style={styles.watchInWstContainer}>
+        <MvButton text={"Visit website"} />
       </View>
     </>
   );
 }
+const screen = Dimensions.get("screen");
+
+const styles = StyleSheet.create({
+  mainPosterImageStyle: {
+    borderBottomLeftRadius: 30,
+    overflow: "hidden",
+    position: "relative",
+  },
+  subPosterContainerStyle: {
+    position: "absolute",
+    bottom: -120,
+    left: 25,
+    borderWidth: 8,
+    borderColor: "#fff",
+    borderRadius: 20,
+    zIndex: 99,
+  },
+  thumbnailStyle: {
+    borderRadius: 20,
+    overflow: "hidden",
+    backgroundColor: "#000",
+  },
+  playButtonStyle: {
+    position: "absolute",
+    bottom: -30,
+    right: 20,
+  },
+  movieHeaderInfoStyle: {
+    paddingLeft: 165,
+    paddingTop: 30,
+    paddingHorizontal: 30,
+  },
+  synopsisStyle: {
+    paddingTop: 40,
+    paddingHorizontal: 25,
+    paddingBottom: 120,
+  },
+  watchInWstContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: screen.width,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+});
